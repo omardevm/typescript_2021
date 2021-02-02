973385d649be276411fcedb1d8f58d9fd50f8a88
work <omardguez.contacto@gmail.com>
2021-02-02 17:50:43 -0600

    module local

--- using-ts.ts
+++ using-ts.ts
@@ -1,11 +1,11 @@
 const button = document.querySelector("#button");
-const input1 = document.querySelector("#num1")!;
-const input2 = document.querySelector("#num2");
+const input1 = document.querySelector("#num1")! as HTMLInputElement;
+const input2 = document.querySelector("#num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
 
 button.addEventListener("click", function () {
   add(input1.value, input2.value);
 });
-
-function add(num1, num2) {
-  return num1 + num2;
-}
