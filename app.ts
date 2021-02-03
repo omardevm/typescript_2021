768fce64facc801a6ea227642423eaa19c8ed5c4
work <omardguez.contacto@gmail.com>
2021-02-03 12:54:50 -0600

    object, array, tuple

--- app.ts
+++ app.ts
@@ -1,1 +1,29 @@
-console.log("Avoid refresh with npm init,npm install --save-dev lite-server .....");
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: (string | number)[];
+} = {
+  name: "Omar",
+  age: 22,
+  hobbies: ["Videogames", "Travel"],
+  role: [2, "author"],
+};
+
+let favoriteActivities: string[] = ["Videogames", "Travel"];
+// Check how in can return number
+for (const hobby in favoriteActivities) {
+  console.log(hobby);
+}
+
+for (const hobby of favoriteActivities) {
+  console.log(hobby.toUpperCase());
+}
+
+favoriteActivities.forEach((hobby) => console.log(hobby));
+
+person.role.push("admin");
+
+person.role[1] = 10;
+
+console.log(person.role);
