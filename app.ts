933146ff3239302feb4f8b058cfff267ad6b056c
work <omardguez.contacto@gmail.com>
2021-02-03 00:19:15 -0600

    npm init, npm install --save-dev lite-server

---
+++ app.ts
@@ -0,0 +1,1 @@
+console.log("Avoid refresh with npm init,npm install --save-dev lite-server .....");
--- using-ts.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("#button");
-var input1 = document.querySelector("#num1");
-var input2 = document.querySelector("#num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    add(+input1.value, +input2.value);
-});
